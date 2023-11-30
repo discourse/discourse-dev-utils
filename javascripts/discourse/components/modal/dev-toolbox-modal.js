df1a63498daf67598b86ffcb188a7091bd2d872b
Kelvin Tan <kelv@discourse.org>
2023-11-30 09:29:22 +0800

    remove redundant modalBody class and lint

--- javascripts/discourse/components/modal/dev-toolbox-modal.js
+++ javascripts/discourse/components/modal/dev-toolbox-modal.js
@@ -1,6 +1,6 @@
-import { action } from "@ember/object";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 import DiscourseURL from "discourse/lib/url";
 
 export default class DevToolboxModal extends Component {
